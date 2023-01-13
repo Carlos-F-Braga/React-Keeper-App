5ae8c456d803fdedad47358dc5f1f8f3fea05152
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2023-01-13 08:22:40 -0300

    notes info programmed

--- src/notes.js
+++ src/notes.js
@@ -3,36 +3,42 @@ const notes = [
       key: 1,
       title: "Delegation",
       content:
-        "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem"
+        "Q. How many programmers does it take to change a light bulb? A. None – It’s a hardware problem",
+      info: 'car[1].description'
     },
     {
       key: 2,
       title: "Loops",
       content:
-        "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat."
+        "How to keep a programmer in the shower forever. Show him the shampoo bottle instructions: Lather. Rinse. Repeat.",
+      info: 'car[2].description'
     },
     {
       key: 3,
       title: "Arrays",
       content:
-        "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
+        "Q. Why did the programmer quit his job? A. Because he didn't get arrays.",
+      info: 'car[3].description'
     },
     {
       key: 4,
       title: "Hardware vs. Software",
       content:
-        "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+        "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software.",
+      info: 'car[4].description'
     },
     {
       key: 5,
       title: "Big ideas",
-      content: "Eat more sushi"
+      content: "Eat more sushi",
+      info: 'car[5].description'
     },
     {
-        key: 5,
+        key: 6,
         title: "BigKing",
-        content: "Be more [Godlike Being]"
+        content: "Be more [Godlike Being]",
+        info: 'car[6].description'
       }
   ];
 
-  export default notes;
+export default notes;
