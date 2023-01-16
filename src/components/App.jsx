61cd89fcd3b5a02fca4422c6ab6eacbe6dad334d
Carlos-F-Braga <carlos.goncalves@agrofauna.com.br>
2023-01-16 19:20:17 -0300

    App js deleting programmed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,12 +14,20 @@ function App() {
     })
   }
 
+  const deleteNote = (id) => {
+    setNotes((currentNotes) => {
+      return currentNotes.filter((noteItem, index) => {
+        return index !== id;
+      })
+    })
+  }
+
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {notes.map((noteItem) => {
-        return <Note title={noteItem.title} content={noteItem.content} />
+      {notes.map((noteItem, index) => {
+        return <Note key={index} id={index} title={noteItem.title} content={noteItem.content} onDelete={deleteNote} />
       })}
       <Footer />
     </div>
